b615cb1001a1d71392416067638bd14421e291f6
Ahmed Junaid <ahmedjunaid42@gmail.com>
2025-02-17 04:51:38 +0500

    seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head'; // Import Head component
 import { ThemeProvider } from '../context/ThemeContext';
 import Preloader from '../components/Preloader';
 import '../styles/globals.css';
@@ -7,13 +8,27 @@ import ChatBot from '../components/ChatBot';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-      <ChatBot />
-      <Preloader />
+    <>
+     <Head>
+  {/* Primary Meta Tags */}
+  <title>Elite Gems Private Limited | Premium Gemstones Worldwide</title>
+  <meta 
+    name="description" 
+    content="Elite Gems Private Limited: Trusted global exporter of authentic  gemstones like Emeralds, Rubies, Sapphires & Topaz. Certified quality, ethical sourcing, and worldwide shipping." 
+  />
 
+  {/* Open Graph (Facebook/LinkedIn) */}
+  <meta property="og:title" content="Elite Gems Private Limited | Premium Gemstones Worldwide" />
+  <meta property="og:image" content="/og-global-gemstones.jpg" />
 
-    </ThemeProvider>
+</Head>
+      
+      <ThemeProvider>
+        <Component {...pageProps} />
+        <ChatBot />
+        <Preloader />
+      </ThemeProvider>
+    </>
   );
 }
 
